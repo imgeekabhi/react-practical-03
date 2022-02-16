362586fcddd3e7778b2e6fa7130234b5866aac21
Abhishek <imgeekabhi@gmail.com>
2022-02-16 15:43:30 +0530

    adding create_react_app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./component/Todo";
+// import GalleryReact from "./component/TabMenu/GalleryReact";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
